7ed465c282912c35f9d2ec5916dd8922a9dd6a15
Nahuel Moreno <nahuel23009@gmail.com>
2024-02-12 17:50:26 -0300

    agregue _redirects.html para que funcionen los links en netlifly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './views/home.js';
 import PDF from "./views/pdf.js";
-import Dino from './views/dino.js';
 import "./App.scss"
 
 function App(){
@@ -10,7 +9,6 @@ function App(){
       <Router>
           <Routes>
             <Route path="/" element={<Home/>} />
-             <Route path="/dino" element={<Dino/>} />
             <Route path="/pdf" element={<PDF/>} />
           </Routes>
       </Router>
